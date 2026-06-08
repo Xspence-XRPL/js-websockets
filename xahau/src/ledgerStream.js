b62f301a717945177c9ca90d20bf93c7638d216c
Andy <handy_4ndy@outlook.com>
2026-06-08 22:16:25 +0100

    Cleanup

--- xahau/src/ledgerStream.js
+++ xahau/src/ledgerStream.js
@@ -1,13 +1,5 @@
 import { Client } from 'xahau';
 
-/*
-The ledger stream only sends ledgerClosed messages when the consensus process declares a new
-validated ledger. The message identifies the ledger and provides some information about its
-contents. 
-
-https://xahau.network/docs/features/http-websocket-apis/public-api-methods/
-*/
-
 // Connect to Xahau and stream validated ledgers
 const client = new Client('wss://xahau.network');
 
