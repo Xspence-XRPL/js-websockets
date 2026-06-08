0179f1bffaf977f85a5f6c4afe92717f01ef7344
Andy <handy_4ndy@outlook.com>
2026-06-08 22:15:09 +0100

    Cleanup

--- xrpl/src/validationStream.js
+++ xrpl/src/validationStream.js
@@ -1,12 +1,5 @@
 import { Client } from 'xrpl';
 
-/*
-Validations stream monitors validation messages (votes) from XRPL validators.
-Sends messages whenever the server receives a validation message during consensus.
-
-https://xrpl.org/docs/references/http-websocket-apis/public-api-methods/subscription-methods/subscribe#validations-stream
-*/
-
 // Connect to XRPL and stream validation messages
 const client = new Client('wss://s2.ripple.com');
 
